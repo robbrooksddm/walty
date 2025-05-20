129b56d20a94c7b789d396f79bf5fb83abdb0810
Rob Brooks <rob.brooks@doubledenimmarketing.co.uk>
2025-05-20 20:23:47 +0100

    chore(test): fix jest transform typing

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+[{
+	"resource": "/Users/robbrooks/Library/CloudStorage/OneDrive-Personal/Unnamed Coding Project/get-card/jest.config.ts",
+	"owner": "typescript",
+	"code": "2322",
+	"severity": 8,
+	"message": "Type '[string]' is not assignable to type 'string | TransformerConfig'.\n  Type '[string]' is not assignable to type 'TransformerConfig'.\n    Source has 1 element(s) but target requires 2.",
+	"source": "ts",
+	"startLineNumber": 6,
+	"startColumn": 16,
+	"endLineNumber": 6,
+	"endColumn": 33,
+	"modelVersionId": 2
+}]
