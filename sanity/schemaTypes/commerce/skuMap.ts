73ff2ecd9fc2694ee653156bd6ba47b27fff86e5
robbrooksddm <rob.brooks@doubledenimmarketing.co.uk>
2025-06-18 17:55:31 +0100

    Remove invalid _id fields from commerce schemas

--- sanity/schemaTypes/commerce/skuMap.ts
+++ sanity/schemaTypes/commerce/skuMap.ts
@@ -6,13 +6,6 @@ export default defineType({
   title: 'SKU mapping',
   fields: [
     defineField({
-      name: '_id',
-      type: 'string',
-      title: 'Document ID',
-      description: 'ID should combine variant and fulfil IDs',
-      validation: r => r.required(),
-    }),
-    defineField({
       name: 'variant',
       type: 'reference',
       title: 'Variant',
