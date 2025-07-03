89220816c281a1a93af3703b680c301ac8fee1c6
robbrooksddm <rob.brooks@doubledenimmarketing.co.uk>
2025-07-03 20:25:42 +0100

    Add rotation handle to image selection

--- lib/fabricDefaults.ts
+++ lib/fabricDefaults.ts
@@ -97,6 +97,8 @@ const utils = (fabric as any).controlsUtils;   // hidden Fabric helpers
 // rotation handle
 (fabric.Object.prototype as any).controls.mtr.render =
   withShadow(utils.renderCircleControl);
+(fabric.Object.prototype as any).controls.mtr.y = 0.5;
+(fabric.Object.prototype as any).controls.mtr.offsetY = 40;
 
 // corner circles
 ['tl','tr','bl','br'].forEach(pos => {
