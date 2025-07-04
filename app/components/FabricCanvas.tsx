337e0c6395bf737eea7571c3f3ad4a96fffdb401
robbrooksddm <rob.brooks@doubledenimmarketing.co.uk>
2025-07-04 23:51:22 +0100

    fix selection overlay flashing

--- app/components/FabricCanvas.tsx
+++ app/components/FabricCanvas.tsx
@@ -1068,6 +1068,7 @@ const drawOverlay = (
     _object?: fabric.Object | null
   }
 ) => {
+  obj.setCoords()
   const box  = obj.getBoundingRect(true, true)
   const rect = canvasRef.current!.getBoundingClientRect()
   const vt   = fc.viewportTransform || [1, 0, 0, 1, 0, 0]
