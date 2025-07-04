ffb3e4345ae602974b8c5d4f2c286fdba60ba673
robbrooksddm <rob.brooks@doubledenimmarketing.co.uk>
2025-07-04 23:05:50 +0100

    Fix overlay sync after transforms

--- app/components/FabricCanvas.tsx
+++ app/components/FabricCanvas.tsx
@@ -1322,24 +1322,24 @@ fc.on('object:moving', () => {
   requestAnimationFrame(() => requestAnimationFrame(syncSel));
 })
 
-  .on('object:modified', () => {
-    if (transformingRef.current) {
-      transformingRef.current = false
-      setActionPos(null)
-      if (actionTimerRef.current) clearTimeout(actionTimerRef.current)
-      actionTimerRef.current = window.setTimeout(() => {
-        requestAnimationFrame(() => requestAnimationFrame(syncSel))
-      }, 250)
-    }
-    hideRotBubble()
-  })
+.on('object:modified', () => {
+  if (transformingRef.current) {
+    transformingRef.current = false
+    setActionPos(null)
+    if (actionTimerRef.current) clearTimeout(actionTimerRef.current)
+    actionTimerRef.current = null
+  }
+  requestAnimationFrame(() => requestAnimationFrame(syncSel))
+  hideRotBubble()
+})
   .on('mouse:up', () => {
     if (transformingRef.current) {
       transformingRef.current = false
       setActionPos(null)
       if (actionTimerRef.current) clearTimeout(actionTimerRef.current)
-      actionTimerRef.current = window.setTimeout(syncSel, 250)
+      actionTimerRef.current = null
     }
+    requestAnimationFrame(() => requestAnimationFrame(syncSel))
     hideSizeBubble()
     hideRotBubble()
   })
