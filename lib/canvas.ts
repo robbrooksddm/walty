0543016b088b4ce5bca38a23682fec10805e470e
robbrooksddm <rob.brooks@doubledenimmarketing.co.uk>
2025-07-16 14:17:23 +0100

    feat: handle missing canvas module

--- lib/canvas.ts
+++ lib/canvas.ts
@@ -1,13 +1,26 @@
 // lib/canvas.ts
 import type * as NodeCanvas from 'canvas'
-const { createCanvas: _createRawCanvas, loadImage } = require('canvas') as typeof NodeCanvas
+
+let canvasLib: typeof NodeCanvas | null = null
+
+try {
+  canvasLib = require('canvas') as typeof NodeCanvas
+} catch {
+  try {
+    const alt = '@napi-rs/canvas'
+    canvasLib = eval('require')(alt) as typeof NodeCanvas
+  } catch {
+    canvasLib = null
+  }
+}
 
 export function createCanvas (width: number, height: number) {
-  const canvas = _createRawCanvas(width, height) as any
+  if (!canvasLib) throw new Error('canvas-not-installed')
+  const canvas = canvasLib.createCanvas(width, height) as any
   canvas.addEventListener ??= () => {}
   canvas.removeEventListener ??= () => {}
   canvas.style ??= {}
   return canvas
 }
 
-export { loadImage }
+export const loadImage = canvasLib?.loadImage as typeof NodeCanvas.loadImage | undefined
