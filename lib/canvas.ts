8c5762b7a62a249ad5c84c36364a3cc81c9614fb
robbrooksddm <rob.brooks@doubledenimmarketing.co.uk>
2025-07-16 14:15:06 +0100

    fix uploads and 3d pipeline

--- lib/canvas.ts
+++ lib/canvas.ts
@@ -1,6 +1,18 @@
 // lib/canvas.ts
 import type * as NodeCanvas from 'canvas'
-const { createCanvas: _createRawCanvas, loadImage } = require('canvas') as typeof NodeCanvas
+
+let CanvasLib: typeof import('canvas') | typeof import('@napi-rs/canvas')
+try {
+  CanvasLib = require('canvas') as typeof NodeCanvas
+} catch {
+  try {
+    CanvasLib = require('@napi-rs/canvas') as typeof NodeCanvas
+  } catch {
+    throw new Error('canvas-not-installed')
+  }
+}
+
+const { createCanvas: _createRawCanvas, loadImage } = CanvasLib
 
 export function createCanvas (width: number, height: number) {
   const canvas = _createRawCanvas(width, height) as any
