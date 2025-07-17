fd279d804ee676b5171164ddf203ae28e85efa8d
robbrooksddm <rob.brooks@doubledenimmarketing.co.uk>
2025-07-17 10:42:37 +0100

    Handle sampler2DArray in shader downgrade

--- app/api/render/route.ts
+++ app/api/render/route.ts
@@ -85,6 +85,7 @@ export async function POST (req: NextRequest) {
             .replace(/^in /gm, 'attribute ')
             .replace(/^out /gm, 'varying ')
             .replace(/sampler3D/g, 'sampler2D')
+            .replace(/sampler2DArray/g, 'sampler2D')
         }
         glContext.shaderSource = (shader: any, src: string) =>
           origShaderSource(shader, downgrade(src))
